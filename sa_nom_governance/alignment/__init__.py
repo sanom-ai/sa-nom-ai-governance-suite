ff904591e5b56a7b8650d8d83a38c408c6c1c8cd
SA-NOM AI <sanomaiarch@gmail.com>
2026-04-02 00:59:26 +0700

    Add Global Harmony baseline loader for v0.6.1

---
+++ sa_nom_governance/alignment/__init__.py
@@ -0,0 +1,8 @@
+from sa_nom_governance.alignment.constitution_models import AlignmentPrinciple, RegionalConstitution
+from sa_nom_governance.alignment.constitution_registry import RegionalConstitutionRegistry
+
+__all__ = [
+    "AlignmentPrinciple",
+    "RegionalConstitution",
+    "RegionalConstitutionRegistry",
+]
